7f1eaf1146fed8de8d9636836a17b0b7af1c06f7
Lady Sun <ladysun.freedom@gmail.com>
2021-06-19 23:30:03 +0200

    [dev] resize screen

--- kodeneko-carousel/src/Carousel.tsx
+++ kodeneko-carousel/src/Carousel.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./App.module.css";
 import cat01 from "./cat01.jpg";
 import cat02 from "./cat02.jpg";
 import cat03 from "./cat03.jpg";
 import cat04 from "./cat04.jpg";
+
+const WIDTH = "1200px";
+const WIDTH_RES = "100vw";
 
 const catImgs = [
   {
@@ -11,15 +14,15 @@ const catImgs = [
     alt: "desc",
   },
   {
-    src: cat02,
+    src: cat01,
     alt: "desc",
   },
   {
-    src: cat03,
+    src: cat01,
     alt: "desc",
   },
   {
-    src: cat04,
+    src: cat01,
     alt: "desc",
   },
 ];
@@ -29,12 +32,21 @@ type StyleList = {
   transition: undefined | string;
 };
 
+type StyleWidth = {
+  cont: number;
+  units: string;
+};
+
+const createValueWidth = (width: StyleWidth, multiply = 1) =>
+  `${width.cont * multiply}${width.units}`;
+
 const Carousel: React.FC = (props) => {
   const [index, setIndex] = useState<number>(0);
   const [movement, setMovement] = useState<StyleList>({
     right: 0,
     transition: "",
   });
+  const [width, setWidth] = useState<StyleWidth>({ cont: 1200, units: "px" });
 
   const handleOnClickArrow = (catIndex: number) => {
     let auxIndex = catIndex;
@@ -45,26 +57,47 @@ const Carousel: React.FC = (props) => {
     setIndex(auxIndex);
   };
 
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(`(max-width: ${createValueWidth(width)})`);
+    const listener = (event: MediaQueryListEvent) => {
+      console.log(event.matches ? "It matches!" : "It doesn't match.");
+      if(event.matches) setWidth({cont: 100, units: 'vw'})
+      else setWidth({cont: 1200, units: 'px'})
+      event.stopPropagation()
+    };
+    mediaQueryList.addEventListener("change", listener);
+  }, [])
+
   return (
-    <div
-      style={{ maxWidth: "1200px", margin: "20px auto", position: "relative" }}
-    >
-      <div className={styles.cont}>
-        <ul className={styles.list} style={movement}>
+    <div style={{ position: "relative" }}>
+      <div
+        className={styles.contImgs}
+        style={{ maxWidth: createValueWidth(width) }}
+      >
+        <ul
+          className={styles.list}
+          style={{
+            ...movement,
+            width: createValueWidth(width, catImgs.length),
+          }}
+        >
           {catImgs.map((cat, index) => (
             <li className={styles.elementList} key={index}>
               <img
                 className={styles.imgEle}
+                style={{ width: createValueWidth(width) }}
                 src={cat.src}
                 alt={cat.alt}
-                width="300px"
               />
             </li>
           ))}
         </ul>
       </div>
 
-      <div className={styles.contDisplay}>
+      <div
+        className={styles.contDisplay}
+        style={{ width: createValueWidth(width) }}
+      >
         <div
           className={styles.left}
           onClick={() => handleOnClickArrow(index - 1)}
@@ -73,10 +106,7 @@ const Carousel: React.FC = (props) => {
         </div>
         <div className={styles.center}>
           {catImgs.map((cat, indexCat) => (
-            <span
-              key={indexCat}
-              onClick={() => handleOnClickArrow(indexCat)}
-            >
+            <span key={indexCat} onClick={() => handleOnClickArrow(indexCat)}>
               •
             </span>
           ))}
