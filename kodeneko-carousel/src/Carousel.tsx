9d7bf9f22aa57c51a1e7758b22732a6f1798a6b1
Lady Sun <ladysun.freedom@gmail.com>
2021-06-19 10:57:57 +0200

    [master] basic movement

--- kodeneko-carousel/src/Carousel.tsx
+++ kodeneko-carousel/src/Carousel.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styles from './App.module.css';
+import React, { useState } from "react";
+import styles from "./App.module.css";
 import cat01 from "./cat01.jpg";
 import cat02 from "./cat02.jpg";
 import cat03 from "./cat03.jpg";
@@ -24,30 +24,61 @@ const catImgs = [
   },
 ];
 
-const Carousel: React.FC = (props) => {
-  return (
-    <div style={{maxWidth: '1200px', margin: '20px auto', position: 'relative'}}>
+type StyleList = {
+  left: undefined | number;
+  right: undefined | number;
+  transition: undefined | string;
+};
 
+const Carousel: React.FC = (props) => {
+  const [movement, setMovement] = useState<StyleList>({
+    left: undefined,
+    right: 0,
+    transition: "",
+  });
+
+  const handleOnClickLeft = () => {
+    const mov = (movement.right as number) - 1200;
+    setMovement({ left: undefined, right: mov, transition: "right 0.5s" });
+  };
+
+  const handleOnClickRight = () => {
+    const mov = (movement.right as number) + 1200;
+    setMovement({ left: undefined, right: mov, transition: "right 0.5s" });
+  };
+
+  return (
+    <div
+      style={{ maxWidth: "1200px", margin: "20px auto", position: "relative" }}
+    >
       <div className={styles.cont}>
-        <ul className={styles.list}>
+        <ul className={styles.list} style={movement}>
           {catImgs.map((cat) => (
             <li className={styles.elementList}>
-              <img className={styles.imgEle} src={cat.src} alt={cat.alt} width="300px" />
+              <img
+                className={styles.imgEle}
+                src={cat.src}
+                alt={cat.alt}
+                width="300px"
+              />
             </li>
           ))}
         </ul>
       </div>
 
       <div className={styles.contDisplay}>
-        <div className={styles.left}>&#60;&#60;</div>
+        <div className={styles.left} onClick={handleOnClickLeft}>
+          &#60;&#60;
+        </div>
         <div className={styles.center}>
           <span>•</span>
           <span>•</span>
           <span>•</span>
         </div>
-        <div className={styles.right}>&#62;&#62;</div>
+        <div className={styles.right} onClick={handleOnClickRight}>
+          &#62;&#62;
+        </div>
       </div>
-
     </div>
   );
 };
