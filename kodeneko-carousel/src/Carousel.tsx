719980c402436f0b066b787cbe05120b4d4565cc
Lady Sun <ladysun.freedom@gmail.com>
2021-06-21 18:32:52 -0400

    [dev] mid fix slider movement

--- kodeneko-carousel/src/Carousel.tsx
+++ kodeneko-carousel/src/Carousel.tsx
@@ -5,9 +5,6 @@ import cat02 from "./cat02.jpg";
 import cat03 from "./cat03.jpg";
 import cat04 from "./cat04.jpg";
 
-const WIDTH = "1200px";
-const WIDTH_RES = "100vw";
-
 const catImgs = [
   {
     src: cat01,
@@ -28,7 +25,7 @@ const catImgs = [
 ];
 
 type StyleList = {
-  right: undefined | number;
+  right: undefined | string;
   transition: undefined | string;
 };
 
@@ -41,32 +38,51 @@ const createValueWidth = (width: StyleWidth, multiply = 1) =>
   `${width.cont * multiply}${width.units}`;
 
 const Carousel: React.FC = (props) => {
-  const [index, setIndex] = useState<number>(0);
+  const [index, _setIndex] = useState<number>(0);
+  const indexeRef = React.useRef(index);
+  const setIndex = (data: number) => {
+    indexeRef.current = data;
+    _setIndex(data);
+  };
+  const [width, setWidth] = useState<StyleWidth>({ cont: 1200, units: "px" });
   const [movement, setMovement] = useState<StyleList>({
-    right: 0,
+    right: "0",
     transition: "",
   });
-  const [width, setWidth] = useState<StyleWidth>({ cont: 1200, units: "px" });
 
-  const handleOnClickArrow = (catIndex: number) => {
+  const handleOnClickArrow = (catIndex: number, actualWidth: StyleWidth) => {
     let auxIndex = catIndex;
     if (catIndex > catImgs.length - 1) auxIndex = 0;
     else if (catIndex < 0) auxIndex = catImgs.length - 1;
-    const mov = auxIndex * 1200;
-    setMovement({ right: mov, transition: "right 0.5s" });
+    const mov = auxIndex * actualWidth.cont;
+    setMovement({
+      right: createValueWidth({ cont: mov, units: actualWidth.units }),
+      transition: "right 0.5s",
+    });
     setIndex(auxIndex);
   };
 
   useEffect(() => {
-    const mediaQueryList = window.matchMedia(`(max-width: ${createValueWidth(width)})`);
+    const mediaQueryList = window.matchMedia(
+      `(max-width: ${createValueWidth(width)})`
+    );
     const listener = (event: MediaQueryListEvent) => {
-      console.log(event.matches ? "It matches!" : "It doesn't match.");
-      if(event.matches) setWidth({cont: 100, units: 'vw'})
-      else setWidth({cont: 1200, units: 'px'})
-      event.stopPropagation()
+        console.log(event)
+      let newWidth: StyleWidth = { cont: 1200, units: "px" };
+      if (event.matches) newWidth = { cont: 100, units: "vw" };
+      setWidth(newWidth);
+      setMovement({
+        right: createValueWidth({
+          cont: newWidth.cont * indexeRef.current,
+          units: newWidth.units,
+        }),
+        transition: "",
+      });
+      event.stopPropagation();
     };
     mediaQueryList.addEventListener("change", listener);
-  }, [])
+    // return () => mediaQueryList.removeEventListener("change", listener);
+  }, []);
 
   return (
     <div style={{ position: "relative" }}>
@@ -100,20 +116,23 @@ const Carousel: React.FC = (props) => {
       >
         <div
           className={styles.left}
-          onClick={() => handleOnClickArrow(index - 1)}
+          onClick={() => handleOnClickArrow(index - 1, width)}
         >
           &#60;&#60;
         </div>
         <div className={styles.center}>
           {catImgs.map((cat, indexCat) => (
-            <span key={indexCat} onClick={() => handleOnClickArrow(indexCat)}>
+            <span
+              key={indexCat}
+              onClick={() => handleOnClickArrow(indexCat, width)}
+            >
               •
             </span>
           ))}
         </div>
         <div
           className={styles.right}
-          onClick={() => handleOnClickArrow(index + 1)}
+          onClick={() => handleOnClickArrow(index + 1, width)}
         >
           &#62;&#62;
         </div>
