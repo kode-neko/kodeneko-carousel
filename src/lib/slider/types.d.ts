011f26074f0ee7035af7ec902a7564eaff6aa2ba
Lady Sun <ladysun.freedom@gmail.com>
2021-06-27 23:57:29 +0200

    [slider] sadly implemented

---
+++ src/lib/slider/types.d.ts
@@ -0,0 +1,17 @@
+type KNSliderCoordinatesX = {
+    initLineX: number;
+    endLineX: number;
+    posDotLeft: number;
+    posDotRight: number;
+    offsetLineLeft: number;
+    widthLine: number;
+  };
+  
+  type KNSliderProps = {
+    initValue?: number;
+    endValue?: number;
+    step?: number;
+    minRangeSeparation?: number;
+    currency?: string;
+  };
+  
