94540321cd4279ae7e04eac8fc2011efd504fc8d
Lady Sun <ladysun.freedom@gmail.com>
2021-06-28 12:40:24 +0200

    [develope] fix export type issue

--- dist/carousel/index.ts
+++ dist/carousel/index.ts
@@ -1,3 +1,3 @@
 export { useKNCarousel } from "./utils";
 export { default as KNCarouselSlides } from "./KNCarouselSlides";
-export type { KNCarouselSlidesProps } from "./types";
+export type { KNCarouselSlidesProps, ImgCarousel } from "./types";
